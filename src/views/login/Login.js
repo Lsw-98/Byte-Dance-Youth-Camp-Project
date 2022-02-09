f8782635f33360dd834e374b085f342957b00170
hollandShaw <holland_shaw@qq.com>
2022-02-10 00:39:52 +0800

    修复粒子效果，网页title修改为News Hub

--- src/views/login/Login.js
+++ src/views/login/Login.js
@@ -3,7 +3,7 @@ import { Form, Input, Button, message } from 'antd';
 import { UserOutlined, LockOutlined } from '@ant-design/icons';
 import './Login.css'
 import axios from 'axios';
-import { tsParticles } from "tsparticles";
+import Particles from 'react-tsparticles';
 
 export default function Login(props) {
   const onFinish = (values) => {
@@ -19,6 +19,161 @@ export default function Login(props) {
 
   return (
     <div style={{ background: "rgb(35, 39, 65)", height: "100%", overflow: "hidden" }} >
+      <Particles height={document.documentElement.clientHeight}params={
+        {
+          "background": {
+            "color": {
+              "value": "rgb(35, 39, 65)"
+            },
+            "position": "50% 50%",
+            "repeat": "no-repeat",
+            "size": "cover"
+          },
+          "fullScreen": {
+            "enable": true,
+            "zIndex": 1
+          },
+          "interactivity": {
+            "events": {
+              "onClick": {
+                "enable": true,
+                "mode": "push"
+              },
+              "onHover": {
+                "enable": true,
+                "mode": "bubble",
+                "parallax": {
+                  "force": 60
+                }
+              }
+            },
+            "modes": {
+              "bubble": {
+                "distance": 400,
+                "duration": 2,
+                "opacity": 1,
+                "size": 40
+              },
+              "grab": {
+                "distance": 400
+              }
+            }
+          },
+          "particles": {
+            "color": {
+              "value": "#ffffff"
+            },
+            "links": {
+              "color": {
+                "value": "#fff"
+              },
+              "distance": 150,
+              "opacity": 0.4
+            },
+            "move": {
+              "attract": {
+                "rotate": {
+                  "x": 600,
+                  "y": 1200
+                }
+              },
+              "enable": true,
+              "outModes": {
+                "default": "bounce",
+                "bottom": "bounce",
+                "left": "bounce",
+                "right": "bounce",
+                "top": "bounce"
+              },
+              "speed": 6
+            },
+            "number": {
+              "density": {
+                "enable": true
+              },
+              "value": 170
+            },
+            "opacity": {
+              "animation": {
+                "speed": 1,
+                "minimumValue": 0.1
+              }
+            },
+            "shape": {
+              "options": {
+                "character": {
+                  "fill": false,
+                  "font": "Verdana",
+                  "style": "",
+                  "value": "*",
+                  "weight": "400"
+                },
+                "char": {
+                  "fill": false,
+                  "font": "Verdana",
+                  "style": "",
+                  "value": "*",
+                  "weight": "400"
+                },
+                "polygon": {
+                  "nb_sides": 5
+                },
+                "star": {
+                  "nb_sides": 5
+                },
+                "image": {
+                  "height": 32,
+                  "replace_color": true,
+                  "src": "/logo192.png",
+                  "width": 32
+                },
+                "images": {
+                  "height": 32,
+                  "replace_color": true,
+                  "src": "/logo192.png",
+                  "width": 32
+                }
+              },
+              "type": "image"
+            },
+            "size": {
+              "value": 16,
+              "animation": {
+                "speed": 40,
+                "minimumValue": 0.1
+              }
+            },
+            "stroke": {
+              "color": {
+                "value": "#000000",
+                "animation": {
+                  "h": {
+                    "count": 0,
+                    "enable": false,
+                    "offset": 0,
+                    "speed": 1,
+                    "sync": true
+                  },
+                  "s": {
+                    "count": 0,
+                    "enable": false,
+                    "offset": 0,
+                    "speed": 1,
+                    "sync": true
+                  },
+                  "l": {
+                    "count": 0,
+                    "enable": false,
+                    "offset": 0,
+                    "speed": 1,
+                    "sync": true
+                  }
+                }
+              }
+            }
+          }
+        }
+      }/>
       <div className='formContainer'>
         <div className="title">全球新闻发布管理系统</div>
         <Form
